4fd1ec3bc68b0bff9f13e325ee0fd9483046a83b
Sukhdeep Singh <143991215+Sukhdeep004@users.noreply.github.com>
2025-07-04 10:18:26 +0530

    Create question2.java

---
+++ question2.java
@@ -0,0 +1,13 @@
+//Question 2
+//Declare and initialize variables for your name, age, and favorite programming language. Print them in a formatted sentence.
+
+
+public class Main {
+    public static void question2(String[] args) {
+        String name = "Sukuu";
+        int age = 23;
+        String favoriteLanguage = "Java";
+
+        System.out.println("My name is " + name + ", I am " + age + " years old, and my favorite programming language is " + favoriteLanguage + ".");
+    }
+}
