bfc5cd277139fd345b5fee45df20c783796c1bdd
Sukhdeep Singh <143991215+Sukhdeep004@users.noreply.github.com>
2025-07-04 14:40:53 +0530

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,31 @@
+//Question 19
+//Write a program that checks whether a number is prime or not using a loop.
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        boolean isPrime = true;
+
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
+
