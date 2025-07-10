2808b02066c9c54f7d33490d0b1379a013f6d3fc
Sukhdeep Singh <143991215+Sukhdeep004@users.noreply.github.com>
2025-07-10 10:44:47 +0530

    Create Q21.java

---
+++ Q21.java
@@ -0,0 +1,18 @@
+//Question 21
+//Write a program that prints Hello, World! to the console.
+//Declare and initialize variables for your name, age, and favorite programming language. Print them in a formatted sentence.
+
+
+public class Q21 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // Declared and initialized variables
+        String name = "SINGH";
+        int age = 25;
+        String favoriteLanguage = "Java";
+
+        // Printed in formatted sentence
+        System.out.println("My name is " + name + ", I am " + age + " years old, and my favorite programming language is " + favoriteLanguage + ".");
+    }
+}
